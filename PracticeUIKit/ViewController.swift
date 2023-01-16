97648905cdfcdb04005c468585d658ad599fbf2a
devanshuchaudhary <dev@sploot.tech>
2023-01-16 14:53:39 +0530

    Initial Commit

---
+++ PracticeUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeUIKit
+//
+//  Created by dddiehard on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
